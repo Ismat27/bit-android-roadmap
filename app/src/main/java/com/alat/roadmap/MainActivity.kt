d751303dee33eadc6e3e8f5cf891100b4c90775c
Ayodeji <ayodeji.olalekan@konga.com>
2024-01-08 10:43:01 +0100

    Base Road Map

---
+++ app/src/main/java/com/alat/roadmap/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.alat.roadmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wema.roadmap.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
