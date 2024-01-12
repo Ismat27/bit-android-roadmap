b96a4f012f8729ef12009c09a74fb6835d78f0fa
ismat27 <adegbiteismail27@gmail.com>
2024-01-12 18:14:20 +0100

    refactor

--- app/src/main/java/com/alat/roadmap/LogoutFragment.kt
+++ app/src/main/java/com/alat/roadmap/LogoutFragment.kt
@@ -2,14 +2,8 @@ package com.alat.roadmap
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import com.alat.roadmap.databinding.FragmentLogoutDialogBinding
 
 
